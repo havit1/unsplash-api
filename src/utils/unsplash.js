dfb986138d975ba296d462be1f4e0fd34738da48
Oleksandr <sashaka2013@gmail.com>
2020-02-14 20:05:20 +0200

    Fixes

--- src/utils/unsplash.js
+++ src/utils/unsplash.js
@@ -4,8 +4,7 @@ export const unsplash = {
   getMainPagePhotos: page => {
     const request = new Request();
     return request.get(`https://api.unsplash.com/photos?page=${page}`, {
-      Authorization:
-        "Client-ID 1daed900b99463debb658164eea9672dc7f95f9939b97a745a479ddbe5592be8"
+      Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_API_KEY}`
     });
   },
   getPhotosByQuery: (qString, page) => {
@@ -16,8 +15,7 @@ export const unsplash = {
     return request.get(
       `https://api.unsplash.com/search/photos?page=1&query=${qString}&page=${page}`,
       {
-        Authorization:
-          "Client-ID 1daed900b99463debb658164eea9672dc7f95f9939b97a745a479ddbe5592be8"
+        Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_API_KEY}`
       }
     );
   }
